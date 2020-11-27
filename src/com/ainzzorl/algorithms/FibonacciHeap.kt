f06c7535166a86eed3d38c6ba27d079626865b3e
ainzzorl <ainzzorl@gmail.com>
2020-11-27 10:54:25 -0800

    Add minimal inefficient implementations of Fibonacci insert and extractMin

--- src/com/ainzzorl/algorithms/FibonacciHeap.kt
+++ src/com/ainzzorl/algorithms/FibonacciHeap.kt
@@ -1,19 +1,39 @@
 package com.ainzzorl.algorithms
 
+import java.util.*
+
 class FibonacciHeap<K : Comparable<K>, V> : Heap<K, V> {
+    private val rootNodes = LinkedList<FibonacciHeapNode<K, V>>()
+    private var minNode: FibonacciHeapNode<K, V>? = null
+
     override fun getMin() : Node<K, V>? {
-        // TODO: implement
-        return null
+        return minNode
     }
 
     override fun extractMin() : Node<K, V>? {
-        // TODO: implement
-        return null
+        val result = minNode
+
+        // TODO: consolidate instead!
+
+        // TODO: not this
+        rootNodes.remove(minNode)
+
+        minNode = null
+        for (rootNode in rootNodes) {
+            if (minNode == null || rootNode.key < minNode!!.key) {
+                minNode = rootNode
+            }
+        }
+
+        return result
     }
 
     override fun insert(key: K, value: V) : Node<K, V> {
         val node = FibonacciHeapNode(key, value)
-        // TODO: implement
+        rootNodes.add(node)
+        if (minNode == null || node.key < minNode!!.key) {
+            minNode = node
+        }
         return node
     }
 
--- src/com/ainzzorl/algorithms/FibonacciHeapNode.kt
+++ src/com/ainzzorl/algorithms/FibonacciHeapNode.kt
@@ -1,4 +1,10 @@
 package com.ainzzorl.algorithms
 
 class FibonacciHeapNode<K: Comparable<K>, V>(override var key: K, override val value: V) : Node<K, V> {
+    var degree = 0
+    var marked = false
+    var parent: FibonacciHeapNode<K, V>? = null
+    var left: FibonacciHeapNode<K, V>? = null
+    var right: FibonacciHeapNode<K, V>? = null
+    var anyChild: FibonacciHeapNode<K, V>? = null
 }
